860dcdcc55dea74c516370731e2408d09e6bdef6
ovp007 <m.albertbb@gmail.com>
2022-02-22 14:19:48 +0100

    initial commit

---
+++ src/main/java/com/milanalbert/luigicoctails/LuigicocktailsApplication.java
@@ -0,0 +1,13 @@
+package com.milanalbert.luigicoctails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuigicocktailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuigicocktailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/milanalbert/luigicoctails/LuigicocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milanalbert.luigicoctails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuigicocktailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
